51a8d81607c21dec678e833bfc473fea4e8dbd40
sergey-as <sergey_as@ukr.net>
2021-04-13 00:47:55 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
+// inputs
+// controlled vs uncontrolled
+// ref
+import React, {useState} from "react";
 import './App.css';
 
+// controlled
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [age, setAge] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        alert(JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            age,
+            password,
+        },null,2))
+    }
+
+    return (
+        <div className="App">
+            <h1>this is input!</h1>
+            <input value={firstName} onChange={({target: {value}}) => setFirstName(value)} type="text" name="firstName" placeholder="enter your first name"/>
+            <br/>
+            <br/>
+            <input value={lastName} onChange={({target: {value}}) => setLastName(value)} type="text" name="lastName" placeholder="enter your last name"/>
+            <br/>
+            <br/>
+            <input value={email} onChange={({target: {value}}) => setEmail(value)} type="email" name="email" placeholder="enter your email"/>
+            <br/>
+            <br/>
+            <input value={age} onChange={({target: {value}}) => setAge(value)} type="number" name="age" placeholder="enter your age"/>
+            <br/>
+            <br/>
+            <input value={password} onChange={({target: {value}}) => setPassword(value)} type="password" name="password" placeholder="enter your password"/>
+            <br/>
+            <br/>
+
+            <button onClick={handleSubmit}>submit</button>
+        </div>
+    );
 }
+
+// uncontrolled
+// function App() {
+//     const firstName = React.useRef();
+//     const lastName = React.useRef();
+//     const email = React.useRef();
+//     const age = React.useRef();
+//     const password = React.useRef();
+//     const form = React.useRef();
+//
+//     // const inputs = React.useRef({
+//     //     firstName: React.useRef(),
+//     //     lastName: React.useRef(),
+//     //     email: React.useRef(),
+//     //     age: React.useRef(),
+//     //     password: React.useRef(),
+//     // })
+//
+//     const onSubmit = (e) => {
+//         e.preventDefault();
+//         console.log(e);
+//
+//         // console.log({firstName});
+//         // firstName.current.value = '';
+//         // firstName.current.focus();
+//         // firstName.current.style.background = 'red';
+//
+//         // 1
+//
+//         // const {
+//         //     target: [
+//         //         {value: firstName},
+//         //         {value: lastName},
+//         //         {value: email},
+//         //         {value: age},
+//         //         {value: password}
+//         //     ],
+//         //     target: {
+//         //         elements: {
+//         //             firstName,
+//         //             lastName,
+//         //             email,
+//         //             age,
+//         //             password,
+//         //         }
+//         //     }
+//         // } = e;
+//
+//         // console.log(target[0]);
+//
+//         // console.log({firstName, lastName, email, age, password});
+//
+//         alert(JSON.stringify({
+//             // firstName, lastName, email, age, password
+//             firstName: firstName.current.value,
+//             lastName: lastName.current.value,
+//             email: email.current.value,
+//             age: age.current.value,
+//             password: password.current.value,
+//         }, null, 2))
+//
+//         // firstName.current.value = '';
+//         // lastName.current.value = '';
+//         // email.current.value = '';
+//         // age.current.value = '';
+//         // password.current.value = '';
+//         form.current.reset();
+//
+//     }
+//
+//     return (
+//         <div className="App">
+//             <h1>this is input!</h1>
+//             {/*<input type="text" placeholder={'hello'} defaultValue="some initial value"/>*/}
+//             {/*react input mask*/}
+//             <form
+//                 ref={form}
+//                 onSubmit={onSubmit}>
+//                 <input ref={firstName} type="text" name="firstName" placeholder="enter your first name"/>
+//                 <br/>
+//                 <br/>
+//                 <input ref={lastName} type="text" name="lastName" placeholder="enter your last name"/>
+//                 <br/>
+//                 <br/>
+//                 <input ref={email} type="email" name="email" placeholder="enter your email"/>
+//                 <br/>
+//                 <br/>
+//                 <input ref={age} type="number" name="age" placeholder="enter your age"/>
+//                 <br/>
+//                 <br/>
+//                 <input ref={password} type="password" name="password" placeholder="enter your password"/>
+//                 <br/>
+//                 <br/>
+//                 {/*<button onClick={onSubmit}>submit</button>*/}
+//                 <button type='submit'>submit</button>
+//             </form>
+//         </div>
+//     );
+// }
 
 export default App;
