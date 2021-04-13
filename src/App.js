291f836874aac63b96d3505f56cebd8bb73b0549
sergey-as <sergey_as@ukr.net>
2021-04-13 20:59:54 +0300

    1234

--- src/App.js
+++ src/App.js
@@ -6,40 +6,86 @@ import './App.css';
 
 // controlled
 function App() {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [age, setAge] = useState('');
-    const [password, setPassword] = useState('');
+    // const [firstName, setFirstName] = useState('');
+    // const [lastName, setLastName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [age, setAge] = useState('');
+    // const [password, setPassword] = useState('');
+
+    // const handleSubmit = () => {
+    //     alert(JSON.stringify({
+    //         firstName,
+    //         lastName,
+    //         email,
+    //         age,
+    //         password,
+    //     },null,2))
+    // }
+
+    const [userData, setUserData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        age: '',
+        password: '',
+    })
+
+    const updateUserData = (e) => {
+        const {target: {value, name}} = e;
+
+        if(name === 'firstName' && value.split(' ').length > 1){
+            setUserData({...userData, [name]: value, lastName:''})
+            return;
+        }
+
+        if(name === 'age' && Number(value) >= 99){
+            console.log(value, typeof value);
+            return;
+        }
+        setUserData({...userData, [name]: value})
+    }
 
     const handleSubmit = () => {
-        alert(JSON.stringify({
-            firstName,
-            lastName,
-            email,
-            age,
-            password,
-        },null,2))
+        alert(JSON.stringify(userData,null,2))
+        setUserData({
+            firstName: '',
+            lastName: '',
+            email: '',
+            age: '',
+            password: '',
+        })
     }
 
     return (
         <div className="App">
             <h1>this is input!</h1>
-            <input value={firstName} onChange={({target: {value}}) => setFirstName(value)} type="text" name="firstName" placeholder="enter your first name"/>
+            {/* <input value={firstName} onChange={({target: {value}}) => setFirstName(value)} type="text" name="firstName" placeholder="enter your first name"/>*/}
+            {/*<input value={lastName} onChange={({target: {value}}) => setLastName(value)} type="text" name="lastName" placeholder="enter your last name"/>*/}
+            {/*<input value={email} onChange={({target: {value}}) => setEmail(value)} type="email" name="email" placeholder="enter your email"/>*/}
+            {/*<input value={age} onChange={({target: {value}}) => setAge(value)} type="number" name="age" placeholder="enter your age"/>*/}
+            {/*<input value={password} onChange={({target: {value}}) => setPassword(value)} type="password" name="password" placeholder="enter your password"/>*/}
+
+            <input autoComplete='none' value={userData.firstName} onChange={updateUserData} type="text" name="firstName" placeholder="enter your first name"/>
             <br/>
             <br/>
-            <input value={lastName} onChange={({target: {value}}) => setLastName(value)} type="text" name="lastName" placeholder="enter your last name"/>
+            <input value={userData.lastName} onChange={updateUserData} type="text" name="lastName" placeholder="enter your last name"/>
             <br/>
             <br/>
-            <input value={email} onChange={({target: {value}}) => setEmail(value)} type="email" name="email" placeholder="enter your email"/>
+            <input value={userData.email} onChange={updateUserData} type="email" name="email" placeholder="enter your email"/>
             <br/>
             <br/>
-            <input value={age} onChange={({target: {value}}) => setAge(value)} type="number" name="age" placeholder="enter your age"/>
+            <input value={userData.age} onChange={updateUserData} type="number" name="age" placeholder="enter your age"/>
             <br/>
             <br/>
-            <input value={password} onChange={({target: {value}}) => setPassword(value)} type="password" name="password" placeholder="enter your password"/>
+            <input value={userData.password} onChange={updateUserData} type="password" name="password" placeholder="enter your password"/>
             <br/>
             <br/>
+
+            {userData.age > 25 ? (
+                <h3>you are old</h3>
+            ) : (
+                <h3>you are old</h3>
+            )}
 
             <button onClick={handleSubmit}>submit</button>
         </div>
